20a31fb57b5adab04dd8b2db4558c6cc925674fa
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-08-16 12:24:39 +0530

    added Car reducer

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const Dummy_product =[
+  {
+    id:'p1', 
+    price:6, 
+    title:'my First Book', 
+    description:"The first book I ever wrote"
+  },
+  {
+    id:'p2', 
+    price:5, 
+    title:'my second Book', 
+    description:"The second book I ever wrote"
+  }
+]
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {Dummy_product.map(product =>
+         <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        /> )}
+       
       </ul>
     </section>
   );
